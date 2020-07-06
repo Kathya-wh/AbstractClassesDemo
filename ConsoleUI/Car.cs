0d5323de3b2d1c1006c3d7d51d847ffb81e84011
Kathya <kathyawhite00@gmail.com>
2020-07-06 14:34:17 -0500

    Completed Abstract Classes

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Car : Vehicles
+    {
+        public Car()
+        {
+        }
+
+        public bool HasTrunk { get; set; } = true;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This car is in drive!");
+        }
+
+
+
+    }
+}
